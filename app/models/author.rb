a929efb3616f3444c7a269e95dc568204baf265c
jqzheng617 <jenna.zheng617@gmail.com>
2019-06-30 15:35:33 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  # error for missing name
+  validates :email, uniqueness: true
+  # error for missing a non-unique email
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  # error for missing title
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  # error for invalid categor
+  validates :content, length: {minimum: 100}
+  # error for too short content
 end
